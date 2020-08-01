d774389dbdfaaca22f523200e722b078c11496f3
HiteshM108 <67020966+HiteshM108@users.noreply.github.com>
2020-08-01 16:24:18 -0500

    -Final Product

--- app.js
+++ app.js
@@ -1,134 +1,185 @@
-box1 = document.getElementById("box1");
-box2 = document.getElementById("box2");
-box3 = document.getElementById("box3");
-box4 = document.getElementById("box4");
-box5 = document.getElementById("box5");
-box6 = document.getElementById("box6");
-box7 = document.getElementById("box7");
-box8 = document.getElementById("box8");
-box9 = document.getElementById("box9");
-outcome = document.getElementById("outcome");
+/*----- constants -----*/
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-var boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+/*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+let win;
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+const msgs = document.querySelector('h2');
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('reset-button').addEventListener('click', init);
+
+/*----- functions -----*/
+function init() {
+    board = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
+    render();
 }
 
-function checkIfEmpty(box) {
-    if (box.innerText == "X" || box.innerText == "O") {
-        return false;
-    } else {
-        return true;
-    }
+function render() {
+    board.forEach(function(mark, index) {
+        squares[index].textContent = mark;
+    });
+    msgs.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 }
 
-function checkIfUserWon() {
-    if (box1.innerText == "X" && box2.innerText == "X" && box3.innerText == "X") {
-        outcome.innerText = "You won!"
-    } else if (box1.innerText == "X" && box2.innerText == "X" && box3.innerText == "X") {
-        outcome.innerText = "You won!"
-    }
-};
-
-function computerChoice() {
-    box1Empty = checkIfEmpty(box1);
-    box2Empty = checkIfEmpty(box2);
-    box3Empty = checkIfEmpty(box3);
-    box4Empty = checkIfEmpty(box4);
-    box5Empty = checkIfEmpty(box5);
-    box6Empty = checkIfEmpty(box6);
-    box7Empty = checkIfEmpty(box7);
-    box8Empty = checkIfEmpty(box8);
-    box9Empty = checkIfEmpty(box9);
-
-    var checkBoxes = [box1Empty, box2Empty, box3Empty, box4Empty, box5Empty, box6Empty, box7Empty, box8Empty, box9Empty];
-
-    var availableBoxes = [];
-
-    var i;
-    for (i = 0; i < 10; i++) {
-        if (checkBoxes[i] == true) {
-            availableBoxes.push(boxes[i]);
-        }
-    }
-
-    var randNum = getRndInteger(0, (availableBoxes.length - 1));
-    availableBoxes[randNum].innerText = "O";
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    })
+    board[idx] = turn;
+    console.log(board);
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render();
 }
 
-function userChoice() {
-    box1Empty = checkIfEmpty(box1);
-    box2Empty = checkIfEmpty(box2);
-    box3Empty = checkIfEmpty(box3);
-    box4Empty = checkIfEmpty(box4);
-    box5Empty = checkIfEmpty(box5);
-    box6Empty = checkIfEmpty(box6);
-    box7Empty = checkIfEmpty(box7);
-    box8Empty = checkIfEmpty(box8);
-    box9Empty = checkIfEmpty(box9);
+function getWinner() {
+    let winner = null;
+    winCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
 
-    var checkBoxes = [box1Empty, box2Empty, box3Empty, box4Empty, box5Empty, box6Empty, box7Empty, box8Empty, box9Empty];
-
-    var availableBoxes = [];
-
-    var i;
-    for (i = 0; i < 10; i++) {
-        if (checkBoxes[i] == true) {
-            availableBoxes.push(boxes[i]);
-        }
-    }
-
-    availableBoxes[0].innerText = "X";
-
-    console.log(availableBoxes[0].innerText);
-
-    var j;
-    for (j = 0; j < availableBoxes.length; j++) {
-        availableBoxes[j].addEventListener("click", function() {
-            availableBoxes[j].innerText = "X";
-            console.log(availableBoxes[j].innerText);
-        })
-    }
+    return winner ? winner : board.includes('') ? null : 'T';
 }
 
-// box1.addEventListener("click", function() {
-//     box1.innerText = "X";
-// })
-// box2.addEventListener("click", function() {
-//     box2.innerText = "X";
-// })
-// box3.addEventListener("click", function() {
-//     box3.innerText = "X";
-// })
-// box4.addEventListener("click", function() {
-//     box4.innerText = "X";
-// })
-// box5.addEventListener("click", function() {
-//     box5.innerText = "X";
-// })
-// box6.addEventListener("click", function() {
-//     box6.innerText = "X";
-// })
-// box7.addEventListener("click", function() {
-//     box7.innerText = "X";
-// })
-// box8.addEventListener("click", function() {
-//     box8.innerText = "X";
-// })
-// box9.addEventListener("click", function() {
-//     box9.innerText = "X";
-// })
 
-computerChoice();
-userChoice();
+init();
 
-// var i;
-// computerChoice();
-// computerChoice();
-// computerChoice();
-// while (i < 10) {
-//     checkIfUserWon();
-//     computerChoice();
-//     i += 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var boxNames = ["box1", "box2", "box3", "box4", "box5", "box6", "box7", "box8", "box9"];
+
+// var boxes = [
+//     [box1, box2, box3],
+// ];
+// for (var i = 0; i < 9; i++) {
+//     boxes.push(document.getElementById(boxNames[i]));
 // }
+
+// function boxClick(box) {
+//     box.innerText = "X";
+// }
+
+// function giveUserOption() {
+//     for (var i = 0; i < 8; i++) {
+//         boxes[i].addEventListener("click", function() {
+//             boxes[i].innerText = "X";
+//         });
+//     }
+
+// boxes[0].addEventListener("click", boxClick(boxes[0]));
+// boxes[1].addEventListener("click", function() {
+//     boxes[1].innerText = "X";
+// });
+// boxes[2].addEventListener("click", function() {
+//     boxes[2].innerText = "X";
+// });
+// boxes[3].addEventListener("click", function() {
+//     boxes[3].innerText = "X";
+// });
+// boxes[4].addEventListener("click", function() {
+//     boxes[4].innerText = "X";
+// });
+// boxes[5].addEventListener("click", function() {
+//     boxes[5].innerText = "X";
+// });
+// boxes[6].addEventListener("click", function() {
+//     boxes[6].innerText = "X";
+// });
+// boxes[7].addEventListener("click", function() {
+//     boxes[7].innerText = "X";
+// });
+// boxes[8].addEventListener("click", function() {
+//     boxes[8].innerText = "X";
+// });
+// }
+
+// function checkIfEmpty(box) {
+//     if (box.innerText == "X" || box.innerText == "O") {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function computerChoice() {
+//     var checkBoxes = []
+//     for (var i = 0; i < 9; i++) {
+//         checkBoxes.push(checkIfEmpty(boxes[i]));
+//     }
+
+//     var availableBoxes = [];
+
+//     for (var i = 0; i < 9; i++) {
+//         if (checkBoxes[i]) {
+//             availableBoxes.push(boxes[i]);
+//         }
+//     }
+
+//     var randNum = getRndInteger(0, (availableBoxes.length - 1));
+//     var chosenBox = availableBoxes[randNum];
+//     chosenBox.innerText = "O";
+// }
+
+// function userChoice() {
+//     var checkBoxes = []
+//     for (var i = 0; i < 9; i++) {
+//         checkBoxes.push(checkIfEmpty(boxes[i]));
+//     }
+
+//     var availableBoxes = [];
+
+//     for (var i = 0; i < 9; i++) {
+//         if (checkBoxes[i]) {
+//             availableBoxes.push(boxes[i]);
+//         }
+//     }
+
+// for (var i = 0; i < availableBoxes.length; i++) {
+//     console.log(availableBoxes[i]);
+//     availableBoxes[i].addEventListener("click", function() {
+//         availableBoxes[i].innerText = "X";
+//     });
+// }
+
+// boxes[0].removeEventListener("click", boxClick(boxes[0]));
+
+
+// }
+
+// giveUserOption();
+// computerChoice();
+// userChoice();
